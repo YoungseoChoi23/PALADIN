defc1180230875885a3938741eed9645bcdd03fc
jimin song <songing01@gmail.com>
2024-05-22 19:23:49 +0900

    ✨ 카카오 로그인 API 연결

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { Route, Routes } from "react-router-dom";
 import RecordDetailPage from "./pages/detailPage/RecordDetailPage";
 import BookDetailPage from "./pages/detailPage/BookDetailPage";
 import GoodsDetailPage from "./pages/detailPage/GoodsDetailPage";
+import LoginLoadingPage from "./pages/LoginLoadingPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/login/loading" element={<LoginLoadingPage />} />
       <Route path="/sinchonMain" element={<SinchonMainPage />} />
       <Route path="/sinchonMain/recordDetail" element={<RecordDetailPage />} />
       <Route path="/sinchonMain/bookDetail" element={<BookDetailPage />} />
--- src/pages/LoginLoadingPage.jsx
+++ src/pages/LoginLoadingPage.jsx
@@ -1,26 +1,22 @@
 import React, { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { postAuthCode } from "../services/api/auth";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const LoginLoadingPage = () => {
-  const location = useLocation();
-  const KAKAO_CODE = location.search.split("=")[1]; // 인가코드
+  const [searchParams, _] = useSearchParams();
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    postAuthCode(KAKAO_CODE)
-      .then(res => {
-        console.log(res);
-        if (res.status === 201) {
-          localStorage.setItem("token", res.data);
-          navigate("/");
-          window.location.reload();
-        }
-      })
-      .catch(err => {
-        alert("로그인에 실패하였습니다.");
-        navigate("/login");
-      });
+    const accessToken = searchParams.get("accessToken");
+
+    if (accessToken) {
+      localStorage.setItem("token", `Bearer ${accessToken}`);
+      navigate("/");
+      window.location.reload();
+    } else {
+      alert("로그인에 실패하였습니다.");
+      navigate("/login");
+    }
   }, []);
 
   return <div>로그인 중...</div>;
