d589f097b456dd7350feac14dd030ead9cbda0e1
Hyerin Sung <hyerinx@naver.com>
2024-05-26 00:10:22 +0900

    ui: 중고매장 메인페이지 배너

---
+++ src/components/MarketMain/Banner.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+import { useState } from "react";
+import { ReactComponent as BtnPrevImg } from "../../assets/MarketMain/main-banner-btn-left.svg";
+import { ReactComponent as BtnNextImg } from "../../assets/MarketMain/main-banner-btn-right.svg";
+
+const Banner = () => {
+  const [bannerImg, setBannerImg] = useState(0);
+
+  function prev() {
+    setBannerImg((bannerImg + 6 - 1) % 6);
+  }
+  function next() {
+    setBannerImg((bannerImg + 1) % 6);
+  }
+
+  return (
+    <Wrapper>
+      <button onClick={prev}>
+        <BtnPrevImg />
+      </button>
+      <img
+        src={require(`../../assets/MarketMain/banner-${bannerImg}.png`)}
+        alt="banner"
+      />
+      <button onClick={next}>
+        <BtnNextImg />
+      </button>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: row nowrap;
+  align-items: center;
+  justify-content: center;
+  gap: 11px;
+
+  button {
+    border: none;
+    outline: none;
+    background: none;
+    padding: 0;
+  }
+`;
+
+export default Banner;
